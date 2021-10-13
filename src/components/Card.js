e6fa9592f8e44c0eb66b7292217901b682584f69
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-10-13 16:23:27 -0300

    Adicionado PropTypes requisito 3

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Card extends React.Component {
   render() {
@@ -48,5 +49,16 @@ class Card extends React.Component {
     );
   }
 }
+
+Card.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+};
 
 export default Card;
